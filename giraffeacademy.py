45e12e2c29e3bcdf7973255c0f614ac781e338ea
Krittika <nettedstars@gmail.com>
2023-11-25 12:21:59 +0000

    string-types-tutorial

---
+++ giraffeacademy.py
@@ -0,0 +1,8 @@
+phrase ="Giraffe Academy"
+print(phrase+" is cool")
+print(phrase.upper().isupper())
+print(f'This phrase is {len(phrase)} characters long') #cannot directly concatenate a string and an integer type, so using f-string
+print(phrase.index("G")) #returns the index of a character in the string where zero is the first position
+print(phrase.replace("Giraffe","Elephant")) #replaces a substring with another substring
+
+
