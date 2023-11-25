364bcfde926a240a7b13f24838f1e0d73a216362
Krittika <nettedstars@gmail.com>
2023-11-25 12:38:13 +0000

    math-function-examples

---
+++ numberexamples.py
@@ -0,0 +1,15 @@
+from math import *
+
+
+my_num=5
+print(str(my_num)+" is my favourite number") #str converts numbers into strings
+my_num= -5
+print(abs(my_num))
+print(pow(4,6))
+print(round(3.2))
+print(round(3.7))
+#functions below need math module
+print(floor(7.9))
+print(ceil(7.8))
+print(sqrt(36))
+print(sqrt(90))
